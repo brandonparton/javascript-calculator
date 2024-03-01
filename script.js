f3029fc4335aae4fa13311c955c40a5fd0cb66ba
Brandon Parton <ssphenic@gmail.com>
2024-03-01 00:15:53 -0600

    Add negative & percent functions. Updated styling

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function calculate() {
         document.getElementById('display').value = 'Error';
     }
 }
+
+function toggleNegative() {
+    let currentValue = document.getElementById('display').value;
+    if (currentValue.startsWith('-')) {
+        document.getElementById('display').value = currentValue.slice(1);
+    } else {
+        document.getElementById('display').value = '-' + currentValue;
+    }
+}
+
+function calculatePercent() {
+    let currentValue = document.getElementById('display').value;
+    if (!currentValue.includes('%')) {
+        document.getElementById('display').value = eval(currentValue) / 100;
+    }
+}
